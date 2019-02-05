c9af36733a1b3438e9caf22d4850a0afc7e0ca4b
taikonavt <taikonavt@gmail.com>
2019-02-05 14:57:18 +0300

    init bottom navigation activity; add 2 different color themes

---
+++ app/src/main/java/com/example/maxim/imageviewer/App.java
@@ -0,0 +1,17 @@
+package com.example.maxim.imageviewer;
+
+import android.app.Application;
+
+public class App extends Application {
+    static private App instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static App getInstance(){
+        return instance;
+    }
+}
